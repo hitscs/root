56885ce99df947d1d5f398444c54d4cd0f248d16
Mingjun.Sun <Mingjun.Sun@CNBJDRCNB079.delta.corp>
2017-04-28 16:42:21 +0800

    改成gradle工程试试？？

---
+++ src/main/groovy/scs/root/Root.groovy
@@ -0,0 +1,5 @@
+package scs.root
+
+class Root {
+
+}
---
+++ src/test/groovy/scs/root/RootTest.groovy
@@ -0,0 +1,14 @@
+package scs.root;
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+class RootTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
